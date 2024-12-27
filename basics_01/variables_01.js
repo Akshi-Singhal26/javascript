f48cc214ddb41357c5a94882bae65af7dcddf3d2
Akshi-Singhal26 <akshi.singhal.ug21@nsut.ac.in>
2024-12-27 08:11:49 +0000

    variables in js

---
+++ basics_01/variables_01.js
@@ -0,0 +1,12 @@
+const account_id = 1445 // cant be chnaged 
+let account_email ="abcd@gmail.com"
+var account_username = "akshi_singhal"
+
+// account_id= 155
+// we cant change value of const 
+
+// console.log(account_id);
+
+let accountstate ; // undefined 
+
+console.table([account_id, account_email, account_username, accountstate]);
