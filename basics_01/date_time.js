cc9f17c4c3a1496bd7a5ec1eaea823aa39a9e80d
Akshi_Singhal <akshi.singhal.ug21@nsut.ac.in>
2024-12-29 17:12:44 +0000

    date and time

---
+++ basics_01/date_time.js
@@ -0,0 +1,22 @@
+// date object => represent a single moment 
+
+const mydate = new Date(); //instance of date obj
+// console.log(mydate);//2024-12-29T16:32:52.351Z
+// console.log(mydate.toString()); //Sun Dec 29 2024 16:36:29 GMT+0000 (Coordinated Universal Time)
+// console.log(mydate.toJSON());//2024-12-29T16:36:51.611Z
+// console.log(mydate.toDateString());//Sun Dec 29 2024
+// console.log(mydate.toISOString());//2024-12-29T16:38:29.934Z
+// console.log(mydate.toLocaleDateString());//12/29/2024
+// console.log(mydate.toLocaleString()); // 12/29/2024, 4:38:29 PM
+// console.log(mydate.toLocaleTimeString());//4:38:29 PM
+// console.log(mydate.toTimeString());//16:38:29 GMT+0000 (Coordinated Universal Time)
+// console.log(mydate.toUTCString());//Sun, 29 Dec 2024 16:38:29 GMT
+
+
+
+
+
+
+
+
+
